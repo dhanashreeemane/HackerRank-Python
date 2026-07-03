d0bdf943b3aa9c8b8f5b3dc1d7687670e095c311
DHANASHREE MANE <dhanashreemane1572@gmail.com>
2026-07-04 01:07:05 +0530

    ✅ Write a function [python3] — approach1

---
+++ solutions/python3/medium/write-a-function/solution-approach1.py
@@ -0,0 +1,20 @@
+# ──────────────────────────────────────────────────
+# Problem     Write a function
+# Difficulty  Medium
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-07-04, 01:06 a.m.
+# ──────────────────────────────────────────────────
+
+def is_leap(year):
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+
