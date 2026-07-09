c647e7e6c3e1686f3ad5f7a6efe1890b569fa727
DHANASHREE MANE <dhanashreemane1572@gmail.com>
2026-07-09 23:07:33 +0530

    ✅ Japanese Cities' Attributes [mysql] — approach1

---
+++ solutions/mysql/easy/japanese-cities-attributes/solution-approach1.sql
@@ -0,0 +1,13 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Japanese Cities' Attributes
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-07-09, 11:07 p.m.
+-- ──────────────────────────────────────────────────
+
+select * 
+from CITY
+where countrycode = "JPN";
